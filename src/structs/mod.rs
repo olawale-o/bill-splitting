c27b50e57585e00838161cd170ac05db5fb44e61
olawale-o <omoogunolawale@gmail.com>
2024-08-31 05:34:06 +0100

    Update structs

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,9 +1,10 @@
+#[derive(Debug, Clone)]
 pub struct User {
     pub name: String,
 }
-
+#[derive(Debug, Clone)]
 pub struct Contributor {
     pub user: User,
-    pub percentage: usize,
+    pub percentage: f32,
     pub amount_contributed: f64,
 }
