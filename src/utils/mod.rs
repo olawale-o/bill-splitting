66d2e322485d56767b4009164220c7459b2b2973
olawale-o <omoogunolawale@gmail.com>
2024-09-02 05:19:42 +0100

    Refactor handle_split function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,12 +2,14 @@ pub mod helpers {
     use crate::structs::Contributor;
 
     pub fn handle_split(contributors: Vec<Contributor>, bill_to_split: &f64) -> Vec<Contributor> {
-        let mut results = vec![];
-        for mut c in contributors {
-            let amount_to_pay = (c.percentage as f64 / 100.0) * bill_to_split;
-            c.amount_contributed = amount_to_pay;
-            results.push(c);
-        }
+        let results: Vec<Contributor> = contributors
+            .into_iter()
+            .map(|mut c| {
+                let amount_to_pay = (c.percentage as f64 / 100.0) * bill_to_split;
+                c.amount_contributed = amount_to_pay;
+                c
+            })
+            .collect();
         results
     }
 }
