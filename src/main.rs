6ae15400e3164562d45933fe8894275a0a2677b5
olawale-o <omoogunolawale@gmail.com>
2024-11-29 04:02:44 +0100

    Add leptos to project

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-use std::str::FromStr;
-mod gui;
-fn main() -> iced::Result {
-    gui::run()
-}
-// fn main() {
-//     if let Err(e) = f64::from_str("0.12") {
-//         println!("Failed conversion to f64: {e}");
-//     } else {
-//         println!("Correct value")
-//     }
-// }
+fn main() {}
