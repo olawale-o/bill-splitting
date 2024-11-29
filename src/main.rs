4ef4a15c34084a48f31cd14825a071610c87e3ab
olawale-o <omoogunolawale@gmail.com>
2024-11-29 04:38:18 +0100

    Setup leptos

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    view! {
+        <h1>Hello world</h1>
+    }
+}
+
+fn main() {
+    mount_to_body(|| view! { <App /> })
+}
